bce9d280ccebae05e6cc5f74fcee8454817e5c6a
krxtch <lorysjele@gmail.com>
2022-07-18 14:39:28 +0200

    Updates

---
+++ precache-manifest.d665aa571a9a54bef4d71c2bbd98cdaf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1783fe7cce24cd90649",
+    "url": "/react-portfolio/static/css/main.515f3807.chunk.css"
+  },
+  {
+    "revision": "b1783fe7cce24cd90649",
+    "url": "/react-portfolio/static/js/main.b1783fe7.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/react-portfolio/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "ef08d93a0b4eb1750b7c",
+    "url": "/react-portfolio/static/js/2.ef08d93a.chunk.js"
+  },
+  {
+    "revision": "bbcd5878fb3300419df9",
+    "url": "/react-portfolio/static/js/runtime~main.bbcd5878.js"
+  },
+  {
+    "revision": "08981c6d3d4966ae75ce12e5e45170c2",
+    "url": "/react-portfolio/static/media/CV.08981c6d.pdf"
+  },
+  {
+    "revision": "580461a33a1a4fe508ec034361fd8b5c",
+    "url": "/react-portfolio/static/media/qrcode.580461a3.svg"
+  },
+  {
+    "revision": "f06a27f722997b69eaf5420f85ebf7a7",
+    "url": "/react-portfolio/static/media/mountain.f06a27f7.jpg"
+  },
+  {
+    "revision": "e5722be07e45c850bc235fb0327f1ad5",
+    "url": "/react-portfolio/static/media/1.e5722be0.jpg"
+  },
+  {
+    "revision": "051e237dc1b3b831a068cd80a004ab42",
+    "url": "/react-portfolio/index.html"
+  }
+];
